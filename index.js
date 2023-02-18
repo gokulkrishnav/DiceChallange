b7c3814a2da24ac87f6f068033f86e28c22b69b7
gokulkrishnav <72247730+gokulkrishnav@users.noreply.github.com>
2023-02-18 15:31:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var images1 = document.querySelectorAll("img")[0];
+// console.log(images1)
+images1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 =  "images/dice" + randomNumber2 + ".png";
+
+var images2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2) {
+
+document.querySelector("h1").innerHTML = " 🚩 PLAYER 1 WINS!";
+}
+
+else if (randomNumber2 > randomNumber1) {
+
+document.querySelector("h1").innerHTML =" player 2 Wins! 🚩";
+}
+
+else {
+
+document.querySelector("h1").innerHTML = "Draw!";
+
+}
